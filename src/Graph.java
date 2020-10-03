ddb9be6a6512470ce2b81b0dc9d7e634d193db3f
Adrien <adrien.tokoglu@etu.univ-amu.fr>
2020-10-03 14:34:18 +0200

    Exo 2 Q1 fini

---
+++ src/Graph.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+
+    private int Nb_sommet;
+    private HashMap<Integer,HashSet<Integer>> adj;
+
+
+    public Graph(int Nb_sommet){
+        this.Nb_sommet = Nb_sommet;
+        adj = new HashMap<>();
+    }
+    public int getNb_sommet() {
+        return Nb_sommet;
+    }
+    public HashMap<Integer, HashSet<Integer>> getAdj() {
+        return adj;
+    }
+
+    void addArc(int source, int destination){
+        if(source >= Nb_sommet || source < 0 || destination >= Nb_sommet || destination < 0){
+            System.out.println("Sommet non disponible sommet compris entre 0 et"+ (Nb_sommet-1) );
+            return;
+        }
+        if(source == destination){
+            System.out.println("Pas d'arc rentrant sur lui meme");
+            return;
+        }
+        if(adj.get(source) == null){
+            adj.put(source,new HashSet<Integer>());
+        }
+        if(adj.get(destination) == null){
+            adj.put(destination,new HashSet<Integer>());
+        }
+        adj.get(source).add(destination);
+        adj.get(destination).add(source);
+
+    }
+
+    void removeArc(int source,int destination){
+        adj.get(source).remove(destination);
+        adj.get(destination).remove(source);
+    }
+
+    boolean chechGraphevide(List<Integer> sous_graphe){
+        if(sous_graphe.size() == Nb_sommet && !adj.isEmpty()){
+            return false;
+        }
+        for(int i : sous_graphe){
+            for(int j : sous_graphe){
+                if (adj.get(i) == null)break;
+                if(i != j && adj.get(i).contains(j)) return false;
+                }
+            }
+
+        return true;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Graph graph = new Graph(7);
+        graph.addArc(0,1);
+        graph.addArc(2,3);
+        graph.addArc(1,3);
+
+        List<Integer> test = new ArrayList<>();
+        test.add(4);
+        test.add(5);
+        test.add(6);
+        if(graph.chechGraphevide(test)) System.out.println("GRAPHE VIDE");
+        test.add(3);
+        test.add(1);
+        if(!graph.chechGraphevide(test)) System.out.println("GRAPHE VIDE PAS VIDE");
+
+
     }
 }
